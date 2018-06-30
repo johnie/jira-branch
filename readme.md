46c34c9659bcb568ea3fd3e487c5a04078bbf822
Johnie Hjelm <johnie.hjelm@mrggroup.com>
2018-06-30 22:39:45 +0200

    Renamed package

--- readme.md
+++ readme.md
@@ -1,11 +1,11 @@
-# jira-git [![Build Status](https://travis-ci.org/johnie/jira-git.svg?branch=master)](https://travis-ci.org/johnie/jira-git)
+# jira-branch [![Build Status](https://travis-ci.org/johnie/jira-branch.svg?branch=master)](https://travis-ci.org/johnie/jira-branch)
 
 > Retrieve JIRA information from current branch
 
 ## Install
 
 ```
-$ npm install jira-git
+$ npm install jira-branch
 ```
 
 ## API
@@ -25,18 +25,18 @@ $ npm install jira-git
 ## Usage
 
 ```js
-const jiraGit = require('jira-git');
+const jiraBranch = require('jira-branch');
 
 const branch = 'feature/XXX-1337-this-is-a-very-important-task';
 
-jiraGit.extract(branch);
+jiraBranch.extract(branch);
 // => {
 //   branch: 'feature/XXX-1337-this-is-a-very-important-task',
 //   type: 'feature',
 //   ticket: 'XXX-1337'
 // }
 
-jiraGit.run().then(data => console.log(data));
+jiraBranch.run().then(data => console.log(data));
 // => {
 //   branch: 'feature/XXX-1337-this-is-a-very-important-task',
 //   type: 'feature',
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import test from 'ava';
-import jiraGit from '.';
+import jiraBranch from '.';
 
 const branch = 'feature/XXX-1337-this-is-a-very-important-task';
 const fake = 'develop';
@@ -11,21 +11,23 @@ const mockBranch = {
 };
 
 test('Should match regex', t => {
-	t.regex(branch, jiraGit.JIRA_REGEX);
+	t.regex(branch, jiraBranch.JIRA_REGEX);
 });
 
 test('Should not match regex', t => {
-	t.notRegex(fake, jiraGit.JIRA_REGEX);
+	t.notRegex(fake, jiraBranch.JIRA_REGEX);
 });
 
 test('Should extract branch info', t => {
-	t.deepEqual(jiraGit.extract(branch), mockBranch);
+	t.deepEqual(jiraBranch.extract(branch), mockBranch);
 });
 
 test('Should match every type', t => {
 	['feature', 'hotfix', 'bugfix', 'release', 'custom'].forEach(type => {
 		t.deepEqual(
-			jiraGit.extract(`${type}/XXX-1337-this-is-a-very-important-task`),
+			jiraBranch.extract(
+				`${type}/XXX-1337-this-is-a-very-important-task`
+			),
 			{
 				branch: `${type}/XXX-1337-this-is-a-very-important-task`,
 				type,
@@ -38,7 +40,9 @@ test('Should match every type', t => {
 test('Should not match type', t => {
 	['develop', 'patch', 'fix', 'bug'].forEach(type => {
 		t.notDeepEqual(
-			jiraGit.extract(`${type}/XXX-1337-this-is-a-very-important-task`),
+			jiraBranch.extract(
+				`${type}/XXX-1337-this-is-a-very-important-task`
+			),
 			{
 				branch: `${type}/XXX-1337-this-is-a-very-important-task`,
 				type,
